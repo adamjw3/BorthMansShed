675fadfd30be67028dc9c7a568f420c6990ef565
adamjw3 <367147+adamjw3@users.noreply.github.com>
2024-01-20 15:39:49 +0000

    change

--- components/ui/date.tsx
+++
@@ -1,9 +0,0 @@
-import {formatInTimeZone} from 'date-fns-tz';
-
-const Date = ({publishedAt}: {publishedAt: string}) => {
-	const date = formatInTimeZone(publishedAt, 'Europe/Amsterdam', 'MMM dd, yyyy');
-
-	return <p className='text-sm'>{date}</p>;
-};
-
-export default Date;
--- lib/utils.ts
+++
@@ -1,6 +0,0 @@
-import {clsx} from 'clsx';
-import type {ClassValue} from 'clsx';
-
-export const cn = (...inputs: ClassValue[]) => {
-	return clsx(inputs);
-};
